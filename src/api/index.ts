60cf65d252882e1ca0c316f035281460c8f5eeed
rpulatov <ufan4ik@mail.ru>
2023-02-24 01:50:37 +0700

    3 files modified

--- src/api/index.ts
+++ src/api/index.ts
@@ -1,8 +1,36 @@
-import * as azdev from 'azure-devops-node-api';
+import * as azapi from 'azure-devops-extension-api';
+import * as SDK from 'azure-devops-extension-sdk';
+import { CoreRestClient, WebApiTeam } from 'azure-devops-extension-api/Core';
+azapi.getClient(CoreRestClient);
 
 const PAT = import.meta.env.VITE_PAT;
+const TOKEN = btoa(`${PAT}:${PAT}`);
 const API_URL = 'https://dev.azure.com';
 const ORG_NAME = 'solution-factory';
 
-let authHandler = azdev.getPersonalAccessTokenHandler(PAT);
-export const client = new azdev.WebApi(`${API_URL}/${ORG_NAME}`, authHandler);
+export function fetchAzure(
+  url: string,
+  options?: {
+    projectId?: string;
+    teamId?: string;
+    parameters?: { [key: string]: string };
+  }
+) {
+  const params = new URLSearchParams([['api-version', '7.0']]);
+  if (options?.parameters) {
+    for (const param in options?.parameters) {
+      params.append(param, options?.parameters[param]);
+    }
+  }
+
+  const projectId = options?.projectId ? options.projectId + '/' : '';
+  const teamId = options?.teamId ? options.teamId + '/' : '';
+
+  return fetch(
+    `${API_URL}/${ORG_NAME}/${projectId}${teamId}/_apis${url}?${params.toString()}`,
+    {
+      method: 'GET',
+      headers: new Headers({ Authorization: `Basic ${TOKEN}` }),
+    }
+  );
+}

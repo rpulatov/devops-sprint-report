d93f3f4839d2faf598127877270f0af56971936f
rpulatov <ufan4ik@mail.ru>
2023-02-28 17:40:44 +0700

    3 files modified

--- src/main.tsx
+++ src/main.tsx
@@ -1,15 +1,14 @@
 import React from 'react';
-import ReactDOM from 'react-dom/client';
+import ReactDOM from 'react-dom';
 import App from './App';
 
 import { SurfaceBackground, SurfaceContext } from 'azure-devops-ui/Surface';
 
 import 'azure-devops-ui/Core/override.css';
 
-ReactDOM.createRoot(document.getElementById('root') as HTMLElement).render(
-  <React.StrictMode>
-    <SurfaceContext.Provider value={{ background: SurfaceBackground.neutral }}>
-      <App />
-    </SurfaceContext.Provider>
-  </React.StrictMode>
+ReactDOM.render(
+  <SurfaceContext.Provider value={{ background: SurfaceBackground.neutral }}>
+    <App />
+  </SurfaceContext.Provider>,
+  document.getElementById('root') as HTMLElement
 );

27318c3db200a0a52ed3def2b573174842ea1ae9
rpulatov <ufan4ik@mail.ru>
2023-03-02 12:33:20 +0700

    1 file modified

--- src/components/SelectIteration.tsx
+++ src/components/SelectIteration.tsx
@@ -20,7 +20,36 @@ export function SelectIteration({ projectId, onSelect }: SelectIterationProps) {
   );
 
   const iterationsSorted = React.useMemo(() => {
-    const items = [...iterations];
+    const locate = 'ru-RU';
+    const options1: Intl.DateTimeFormatOptions = {
+      month: '2-digit',
+      day: 'numeric',
+    };
+
+    const options2: Intl.DateTimeFormatOptions = {
+      month: '2-digit',
+      day: 'numeric',
+      year: 'numeric',
+    };
+
+    const now = Date.now();
+
+    const items = iterations.map((item) => {
+      const startDateObj = new Date(item.attributes.startDate);
+      startDateObj.setUTCHours(0, 0, 0, 0);
+
+      const finishDateObj = new Date(item.attributes.finishDate);
+      finishDateObj.setUTCHours(23, 59, 59, 999);
+
+      return {
+        ...item,
+        startDate: startDateObj.toLocaleDateString(locate, options1),
+        finishDate: finishDateObj.toLocaleDateString(locate, options2),
+        currentSprint:
+          Number(now) <= Number(finishDateObj) &&
+          Number(now) >= Number(startDateObj),
+      };
+    });
     return items.reverse();
   }, [iterations]);
 
@@ -28,8 +57,12 @@ export function SelectIteration({ projectId, onSelect }: SelectIterationProps) {
     <select onChange={onChange}>
       <option value={undefined}>--</option>
       {iterationsSorted.map((item) => (
-        <option key={item.id} value={item.id}>
-          {item.name}
+        <option
+          key={item.id}
+          value={item.id}
+          style={{ backgroundColor: item.currentSprint ? '#8ac4ff' : '' }}
+        >
+          {item.name} [{item.startDate} - {item.finishDate}]
         </option>
       ))}
     </select>

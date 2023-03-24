71bd1b94a46db0b8fd05b15bf9cbf3c2f3042572
rpulatov <ufan4ik@mail.ru>
2023-03-24 12:00:06 +0700

    1 file modified

--- src/components/TeamReport.tsx
+++ src/components/TeamReport.tsx
@@ -1,8 +1,55 @@
 import { TeamMemberCapacity } from 'azure-devops-extension-api/Work';
-import { useMemo } from 'react';
+import { useMemo, useCallback } from 'react';
 import { WorkItemState } from '../domains/workItems';
 import { TypeReport } from '../types/report';
 import { diffInDays } from '../utils';
+
+type TeamReportRow = {
+  id: string;
+  name: string;
+  capacity: number;
+  planEstimate: number;
+  planComplete: number;
+  planRemaining: number;
+  overplanEstimate: number;
+  overplanComplete: number;
+  overplanRemaining: number;
+};
+
+const calcPercentageLoad = (
+  item: Pick<TeamReportRow, 'capacity' | 'planEstimate'>
+) => {
+  if (item.capacity <= 0) return '-';
+  return `${((item.planEstimate * 100) / item.capacity).toFixed(0)}%`;
+};
+
+const calcEstimationAccuracy = (
+  item: Pick<TeamReportRow, 'planEstimate' | 'planComplete' | 'planRemaining'>
+) => {
+  if (item.planEstimate <= 0) return '-';
+  const accuracy =
+    ((item.planComplete + item.planRemaining) * 100) / item.planEstimate - 100;
+
+  return accuracy === 0
+    ? `идеально`
+    : accuracy > 0
+    ? `на ${accuracy.toFixed(0)}% дольше`
+    : `на ${-accuracy.toFixed(0)}% быстрее`;
+};
+
+const calcPercentageProductivity = (
+  item: Pick<TeamReportRow, 'planComplete' | 'overplanComplete' | 'capacity'>
+) => {
+  if (item.capacity <= 0) return '-';
+  const accuracy =
+    ((item.planComplete + item.overplanComplete) * 100) / item.capacity - 100;
+
+  return accuracy === 0
+    ? `идеально`
+    : accuracy > 0
+    ? `переработка ${accuracy.toFixed(0)}%`
+    : `недоработка ${-accuracy.toFixed(0)}%`;
+};
 
 type TeamReportProps = {
   teamMembers: TeamMemberCapacity[];
@@ -39,18 +86,6 @@ export function TeamReport({
   }, [teamMembers, iterationDays]);
 
   const teamReport = useMemo(() => {
-    type TeamReportRow = {
-      id: string;
-      name: string;
-      capacity: number;
-      planEstimate: number;
-      planComplete: number;
-      planRemaining: number;
-      overplanEstimate: number;
-      overplanComplete: number;
-      overplanRemaining: number;
-    };
-
     const teamReportObj = teamCapacity.reduce(
       (acc: { [key: string]: TeamReportRow }, item) => {
         if (item.capacity > 0)
@@ -85,6 +120,35 @@ export function TeamReport({
     return Object.values(teamReportObj);
   }, [workItems, teamCapacity]);
 
+  const total = useMemo(
+    () =>
+      teamReport.reduce(
+        (acc, item) => {
+          acc.capacity += item.capacity;
+          acc.planEstimate += item.planEstimate;
+          acc.planComplete += item.planComplete;
+          acc.planRemaining += item.planRemaining;
+          acc.overplanComplete += item.overplanComplete;
+          acc.overplanEstimate += item.overplanEstimate;
+          acc.overplanRemaining += item.overplanRemaining;
+          return acc;
+        },
+        {
+          capacity: 0,
+          planEstimate: 0,
+          planComplete: 0,
+          planRemaining: 0,
+          overplanComplete: 0,
+          overplanEstimate: 0,
+          overplanRemaining: 0,
+          percentageLoad: 0,
+          estimationAccuracy: 0,
+          percentageProductivity: 0,
+        }
+      ),
+    [teamReport]
+  );
+
   return (
     <table>
       <thead>
@@ -113,53 +177,58 @@ export function TeamReport({
         </tr>
       </thead>
       <tbody>
-        {teamReport.map((item) => (
-          <tr key={item.id}>
-            <td>{item.name}</td>
-            <td>{item.capacity.toFixed(1)}</td>
+        {teamReport.map((item) => {
+          return (
+            <tr key={item.id}>
+              <td>{item.name}</td>
+              <td>{item.capacity.toFixed(1)}</td>
 
-            <td>{item.planEstimate.toFixed(1)}</td>
-            {typeReport === TypeReport.SprintResult ? (
-              <>
-                <td>{item.planComplete.toFixed(1)}</td>
-                <td>{item.planRemaining.toFixed(1)}</td>
+              <td>{item.planEstimate.toFixed(1)}</td>
+              {typeReport === TypeReport.SprintResult ? (
+                <>
+                  <td>{item.planComplete.toFixed(1)}</td>
+                  <td>{item.planRemaining.toFixed(1)}</td>
 
-                <td>{item.overplanComplete.toFixed(1)}</td>
-                <td>{item.overplanEstimate.toFixed(1)}</td>
-                <td>{item.overplanRemaining.toFixed(1)}</td>
-              </>
-            ) : null}
-            <td>
-              {item.capacity > 0
-                ? `${((item.planEstimate * 100) / item.capacity).toFixed(0)}%`
-                : '-'}
-            </td>
-            {typeReport === TypeReport.SprintResult ? (
-              <>
-                <td>
-                  {item.planEstimate > 0
-                    ? (
-                        ((item.planComplete + item.planRemaining) * 100) /
-                          item.planEstimate -
-                        100
-                      ).toFixed(0) + '%'
-                    : '-'}
-                </td>
-                <td>
-                  {' '}
-                  {item.planEstimate > 0
-                    ? (
-                        ((item.planComplete + item.overplanComplete) * 100) /
-                          item.planEstimate -
-                        100
-                      ).toFixed(0) + '%'
-                    : '-'}
-                </td>
-              </>
-            ) : null}
-          </tr>
-        ))}
+                  <td>{item.overplanComplete.toFixed(1)}</td>
+                  <td>{item.overplanEstimate.toFixed(1)}</td>
+                  <td>{item.overplanRemaining.toFixed(1)}</td>
+                </>
+              ) : null}
+              <td>{calcPercentageLoad(item)}</td>
+              {typeReport === TypeReport.SprintResult ? (
+                <>
+                  <td>{calcEstimationAccuracy(item)}</td>
+                  <td>{calcPercentageProductivity(item)}</td>
+                </>
+              ) : null}
+            </tr>
+          );
+        })}
       </tbody>
+      <tfoot>
+        <tr>
+          <td>ИТОГО:</td>
+          <td>{total.capacity.toFixed(1)}</td>
+          <td>{total.planEstimate.toFixed(1)}</td>
+          {typeReport === TypeReport.SprintResult ? (
+            <>
+              <td>{total.planComplete.toFixed(1)}</td>
+              <td>{total.planRemaining.toFixed(1)}</td>
+
+              <td>{total.overplanComplete.toFixed(1)}</td>
+              <td>{total.overplanEstimate.toFixed(1)}</td>
+              <td>{total.overplanRemaining.toFixed(1)}</td>
+            </>
+          ) : null}
+          <td>{calcPercentageLoad(total)}</td>
+          {typeReport === TypeReport.SprintResult ? (
+            <>
+              <td>{calcEstimationAccuracy(total)}</td>
+              <td>{calcPercentageProductivity(total)}</td>
+            </>
+          ) : null}
+        </tr>
+      </tfoot>
     </table>
   );
 }

5aa9283c6c9b96f812bbcbe9d4cb2cefeae3b240
rpulatov <r.pulatov@solutionfactory.ru>
2023-10-03 15:33:50 +0700

    add prettier

--- vite.config.ts
+++ vite.config.ts
@@ -1,10 +1,10 @@
-import { defineConfig, loadEnv } from "vite";
-import react from "@vitejs/plugin-react";
+import react from "@vitejs/plugin-react"
+import { defineConfig, loadEnv } from "vite"
 
 export default defineConfig(({ mode }) => {
-  const env = loadEnv(mode, process.cwd(), "");
+  const env = loadEnv(mode, process.cwd(), "")
   return {
     plugins: [react()],
-    base: env.VITE_BASE_URL,    
-  };
-});
+    base: env.VITE_BASE_URL,
+  }
+})
